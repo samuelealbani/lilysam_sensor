db1e3f951967f75dd0bfabd9e51b743ddd890950
samuele <samuelealbani@gmail.com>
2023-05-07 19:41:48 +0100

    add timer and counter

--- pico-thebeams.py
+++ pico-thebeams.py
@@ -1,7 +1,12 @@
 import machine
+from machine import Timer
 import urequests as requests
 import network
 import time
+
+timer = Timer()
+
+counter=0
 
 
 # Wifi Credentials
@@ -23,14 +28,12 @@ def connect_to_wifi(ssid, psk):
         raise Exception("Wifi not available")
     print("Connected to WiFi")
 
-
-try:
-    connect_to_wifi(SSID, PSK)
-
+def addTest(timer):
+    global counter
     url = "https://eu-west-2.aws.data.mongodb-api.com/app/data-mxvxa/endpoint/data/v1/action/insertOne"
     headers = { "api-key": "LVrqps10Se46eEZjOmT20mGYLQaOPyLviQ3fnhGxZU4zqD4WEBsakffxHWn4M8W9" }
 
-    documentToAdd = {"device": "MyPico", "readings": [1, 3, 1, 2, 6, 2, 6]}
+    documentToAdd = {"device": "MyPico", "readings": counter}
 
     insertPayload = {
         "dataSource": "SensorsCluster0",
@@ -52,12 +55,44 @@ try:
 
     # Always close response objects so we don't leak memory
     response.close()
+    counter=counter+1
+
+try:
+    connect_to_wifi(SSID, PSK)
+    
+    timer.init(freq=2, mode=Timer.PERIODIC, callback=addTest)
+
+#     url = "https://eu-west-2.aws.data.mongodb-api.com/app/data-mxvxa/endpoint/data/v1/action/insertOne"
+#     headers = { "api-key": "LVrqps10Se46eEZjOmT20mGYLQaOPyLviQ3fnhGxZU4zqD4WEBsakffxHWn4M8W9" }
+# 
+#     documentToAdd = {"device": "MyPico", "readings": [1, 3, 1, 2, 6, 2, 6]}
+# 
+#     insertPayload = {
+#         "dataSource": "SensorsCluster0",
+#         "database": "myDatabase",
+#         "collection": "recipes",
+#         "document": documentToAdd,
+#     }
+# 
+#     print("sending...")
+# 
+#     response = requests.post(url, headers=headers, json=insertPayload)
+# 
+#     print("Response: (" + str(response.status_code) + "), msg = " + str(response.text))
+# 
+#     if response.status_code == 201:
+#         print("Added Successfully")
+#     else:
+#         print("Error")
+# 
+#     # Always close response objects so we don't leak memory
+#     response.close()
 
 except Exception as e:
     print(e)
 
 # Stop the Wifi
-try:
-    wlan.active(False)
-except Exception as e:
-    print(e)
+# try:
+#     wlan.active(False)
+# except Exception as e:
+#     print(e)
